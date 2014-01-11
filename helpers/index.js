0a7861b915293aa01b6e0e5fbacc6dc60f3db8cf
Choi Goon-ho <caiguanhao@gmail.com>
2014-01-11 15:26:16 +0800

    add new template and reverse methods;

--- helpers/index.js
+++ helpers/index.js
@@ -56,5 +56,40 @@ module.exports.register = function(Handlebars, options) {
     } else {
       return content.inverse(this);
     }
-  })
+  });
+
+  Handlebars.registerHelper('eachReverse', function(context) {
+    var options = arguments[arguments.length - 1];
+    var ret = '';
+
+    if (context && context.length > 0) {
+        for (var i = context.length - 1; i >= 0; i--) {
+            ret += options.fn(context[i]);
+        }
+    } else {
+        ret = options.inverse(this);
+    }
+
+    return ret;
+  });
+
+  Handlebars.registerHelper('eachPropertyReverse', function(context, options) {
+    var content = (function () {
+      var keys = [];
+      for (var key in context) {
+        keys.unshift(key);
+      }
+      var results = [];
+      for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        var value = context[key];
+        results.push(options.fn({
+          key: key,
+          value: value
+        }));
+      }
+      return results;
+    })();
+    return content.join('');
+  });
 };
